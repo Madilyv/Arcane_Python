8161466b2d06b88aae322b6b7f8403842bc6966a
Ruggie <shaun.m.bridges@gmail.com>
2025-07-04 13:00:11 -0400

    Break: adding FWA Subcommands and breaking things

---
+++ extensions/commands/fwa/bases.py
@@ -0,0 +1,65 @@
+from . import loader, fwa  # Use shared loader & group
+
+import uuid
+import lightbulb
+import hikari
+
+from . import loader, fwa
+from utils.mongo import MongoClient
+from extensions.factories.fwa_bases import get_fwa_base_object
+from utils.constants import BLUE_ACCENT
+from utils.emoji import emojis
+
+from hikari.impl import (
+    MessageActionRowBuilder as ActionRow,
+    TextSelectMenuBuilder as TextSelectMenu,
+    SelectOptionBuilder as SelectOption,
+    ContainerComponentBuilder as Container,
+    TextDisplayComponentBuilder as Text,
+    MediaGalleryComponentBuilder as Media,
+    MediaGalleryItemBuilder as MediaItem,
+)
+print("loaded")
+
+@loader.command
+@fwa.register
+class Bases(lightbulb.SlashCommand,
+            name="bases",
+            description="Select and display an FWA base Town Hall level"):
+
+    @lightbulb.invoke
+    @lightbulb.di.with_di
+    async def invoke(self, ctx: lightbulb.Context, mongo: MongoClient = lightbulb.di.INJECTED):
+        action_id = str(uuid.uuid4())
+        print("HI")
+        components = [
+            Container(
+                accent_color=BLUE_ACCENT,
+                components=[
+                    Text(content="## Select FWA Base Town Hall Level"),
+                    Text(content="Use the dropdown menu below to assign the appropriate Town Hall level for the recruit."),
+                    # ActionRow(components=[
+                    #     TextSelectMenu(
+                    #         max_values=1,
+                    #         custom_id=f"th_select:{action_id}",
+                    #         placeholder="Select a Base...",
+                    #         options=[
+                    #             SelectOption(emoji=emojis.TH17.partial_emoji, label="TH17", value="th17"),
+                    #             SelectOption(emoji=emojis.TH16.partial_emoji, label="TH16", value="th16"),
+                    #             SelectOption(emoji=emojis.TH15.partial_emoji, label="TH15", value="th15"),
+                    #             SelectOption(emoji=emojis.TH14.partial_emoji, label="TH14", value="th14"),
+                    #             SelectOption(emoji=emojis.TH13.partial_emoji, label="TH13", value="th13"),
+                    #             SelectOption(emoji=emojis.TH12.partial_emoji, label="TH12", value="th12"),
+                    #             SelectOption(emoji=emojis.TH11.partial_emoji, label="TH11", value="th11"),
+                    #             SelectOption(emoji=emojis.TH10.partial_emoji, label="TH10", value="th10"),
+                    #             SelectOption(emoji=emojis.TH9.partial_emoji, label="TH9", value="th9"),
+                    #         ],
+                    #     )
+                    # ]),
+                    Media(items=[MediaItem(media="assets/Blue_Footer.png")]),
+                    Text(content=f"_Requested by {ctx.member.display_name if ctx.member else ctx.user.username}_"),
+                ]
+            )
+        ]
+
+        await ctx.respond(components=components, ephemeral=True)
