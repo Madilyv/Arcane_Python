8cef7bebf3cac051c25dae712e3bf77cb6776693
Ruggie <shaun.m.bridges@gmail.com>
2025-10-17 16:54:59 +0100

    fix: add missing import for update_clan_info to register dashboard actions

--- extensions/commands/clan/dashboard/__init__.py
+++ extensions/commands/clan/dashboard/__init__.py
@@ -1,6 +1,6 @@
 from extensions.commands.clan import loader, clan
 from .dashboard import dashboard_page
 from .points import *
-from . import fwa_data
+from . import fwa_data, update_clan_info
 
 __all__ = ["dashboard_page"]
